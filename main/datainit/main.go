42f19cba088fbcab1324dd2495c82baa21bd0b7f
jixiufeng <jixiufeng@luojilab.com>
2018-04-04 01:07:05 +0800

    datainit quick

--- main/datainit/main.go
+++ main/datainit/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"time"
 
@@ -20,31 +21,46 @@ func initFundValueHistory() {
 	dbT, _ := dt.NewDatabaseTemplateWithConfig(defs.DBConfig, true)
 	db.FundValueHistoryCreateTable(dbT)
 	stockList := eastmoney.GetFundIdList(eastmoney.FundTypeAll)
-	var tryAgainList []string
+	// var tryAgainList []string
+	var workerCnt = 100
+	var queue [][]eastmoney.FundBase = make([][]eastmoney.FundBase, workerCnt)
 	for idx, fb := range stockList {
-		fmt.Printf("%d/%d id=%s\n", idx, len(stockList), fb.Id)
-		fd, err := eastmoney.GetFund(fb.Id, 0)
-		if err != nil {
-			fmt.Println(err)
-			tryAgainList = append(tryAgainList, fd.Id)
-			continue
-		}
-		db.FundValueHistoryInsertAll(dbT, fd)
-		time.Sleep(time.Millisecond * 100)
+		queue[idx%workerCnt] = append(queue[idx%workerCnt], fb)
 	}
+	var waitGroup sync.WaitGroup
+	for _, q := range queue {
+		waitGroup.Add(1)
+		go func(q []eastmoney.FundBase) {
+			for idx, fb := range q {
+				fmt.Printf("%d/%d id=%s\n", idx, len(q), fb.Id)
+				fd, err := eastmoney.GetFund(fb.Id, 0)
+				if err != nil {
+					fmt.Println(err)
+					// tryAgainList = append(tryAgainList, fd.Id)
+					continue
+				}
+				db.FundValueHistoryInsertAll(dbT, fd)
+				time.Sleep(time.Millisecond * 10)
+			}
+			waitGroup.Done()
+
+		}(q)
+
+	}
+	waitGroup.Wait()
 	fmt.Println("failed_list", tryAgainList)
 
-	for idx, fundId := range tryAgainList {
+	// for idx, fundId := range tryAgainList {
 
-		fmt.Printf("%d/%d id=%s\n", idx, len(tryAgainList), fundId)
-		fd, err := eastmoney.GetFund(fundId, 0)
-		if err != nil {
-			fmt.Println(err)
-			tryAgainList = append(tryAgainList, fundId)
-			continue
-		}
-		db.FundValueHistoryInsertAll(dbT, fd)
-		time.Sleep(time.Millisecond * 100)
-	}
+	// 	fmt.Printf("%d/%d id=%s\n", idx, len(tryAgainList), fundId)
+	// 	fd, err := eastmoney.GetFund(fundId, 0)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		tryAgainList = append(tryAgainList, fundId)
+	// 		continue
+	// 	}
+	// 	db.FundValueHistoryInsertAll(dbT, fd)
+	// 	time.Sleep(time.Millisecond * 10)
+	// }
 
 }
